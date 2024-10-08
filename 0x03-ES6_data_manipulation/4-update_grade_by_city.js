bc5c388b14233416efd4bdee9325a9afcd7ca149
kamelx0 <alesaweyk@gmail.com>
2024-10-09 00:16:02 +0300

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,37 +1,15 @@
-module.exports = {
-  env: {
-    browser: false,
-    es6: true,
-    jest: true,
-  },
-  extends: [
-    'airbnb-base',
-    'plugin:jest/all',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['jest'],
-  rules: {
-    'max-classes-per-file': 'off',
-    'no-underscore-dangle': 'off',
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
-    ],
-  },
-  overrides:[
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
-    }
-  ]
-};
+export default function updateStudentGradeByCity(students, city, grades) {
+  const newStudents = students.filter((student) => student.location === city)
+    .map((student) => {
+      const newStudent = { ...student };
+      for (const grade of grades) {
+        if (grade.studentId === student.id) {
+          newStudent.grade = grade.grade;
+          return newStudent;
+        }
+      }
+      newStudent.grade = 'N/A';
+      return newStudent;
+    });
+  return newStudents;
+}
